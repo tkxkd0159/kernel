be2053a90afdc87596ef1ad01071af78cf44de8b
ljs_ku_linux <41176085+tkxkd0159@users.noreply.github.com>
2021-01-27 14:02:43 +0900

    Update myqemu.sh

--- myqemu.sh
+++ myqemu.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # remove the colons from the options that don't require arguments
 
-usage() { echo "Usage: $0 [-c : create] [-i <qemu-img>] [-w <working directory>] " 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-c : create,[-o <OS-img>]] [-i <qemu-img>] [-w <working directory>] [-m <int> : memory] " 1>&2; exit 1; }
+
+#OPT_BOOL=${1:-False}
 
 OPTIND=1
 CREATE=0
-IMG="ubuntu_test.img"
+IMG="ubuntu100.img"
 WD="kernel_test"
 OS="ubuntu-20.04.1-desktop-amd64.iso"
+SMP=4
+RAM=4096
 
-while getopts "h?ci:w:o:" opt; do
+while getopts "h?ci:w:o:m:" opt; do
 	case "$opt" in
 		h|\?)
 			usage
@@ -24,16 +28,24 @@ while getopts "h?ci:w:o:" opt; do
 			WD=${OPTARG}
 			;;
 		o)
-			OS=${OPTARG}	
+			OS=${OPTARG}
+			;;
+		m)
+			RAM=${OPTARG:-$RAM}	
 	esac
 done
 
 shift $((OPTIND-1))
 
+echo "isCreate=$CREATE, qemu-img : ${IMG}, RAM :${RAM}, nproc:${SMP}"
+
 if [ $CREATE = true ]; then
 	sudo qemu-system-x86_64 \
+		--enable-kvm \
 		-name "ljsvm" \
-		-m 4096 \
+		-cpu host \
+		-smp ${SMP} \
+		-m ${RAM} \
 		-drive file=/home/$(users)/${WD}/${IMG},if=virtio \
 		-cdrom /home/$(users)/${WD}/${OS} \
 		-rtc base=localtime \
@@ -44,12 +56,11 @@ else
 	sudo qemu-system-x86_64 \
 		--enable-kvm \
 		-name "ljsvm" \
-		-m 4096 \
+		-cpu host \
+		-smp ${SMP} \
+		-m ${RAM} \
 		-drive file=/home/$(users)/${WD}/${IMG},if=virtio \
-#		-cdrom /home/$(users)/${WD}/${OS} \
 		-rtc base=localtime \
 		-vga virtio \
 		-device virtio-keyboard-pci
 fi
-
-echo "isCreate=$CREATE leftovers: $@ "
