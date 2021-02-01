760286b5f61e49a4d7545ca8aa5b3f70a68c86e9
ljs_ku_linux <41176085+tkxkd0159@users.noreply.github.com>
2021-02-01 18:51:49 +0900

    Update new VM generation script(virsh)

---
+++ virt_install.sh
@@ -0,0 +1,20 @@
+# virt-viewer
+# virsh list --all
+# virsh shutdown <VM>
+# virsh start <VM>
+# virsh destroy <VM>
+# virsh reboot <VM>
+
+# virsh undefine <VM>
+# virsh define filename.xml
+
+UPPER_DIR=$( pwd -P )
+echo${UPPER_DIR}
+
+virt-install \
+--name focal2004 \
+--ram 4096 \
+--vcpus 2 \
+--disk ${UPPER_DIR}/ubuntu_test.img \
+--import \
+--os-variant ubuntu20.04 
